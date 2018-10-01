21a9340cd2b97ce84c805b7aff9d425d2cb3eff0
LAPTOP-1L0MPP9Q\demok <ch3067@nyu.edu>
2018-09-30 22:39:55 -0400

    New Commit

--- Freeway Assignment/Assets/Player1.cs
+++ Freeway Assignment/Assets/Player1.cs
@@ -20,24 +20,24 @@ public class Player1 : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+		if (Input.GetKey(KeyCode.UpArrow))
 		{
 			transform.position = new Vector3(transform.position.x,transform.position.y + Speed, transform.position.z);
 		}
 
-		if (Input.GetKeyDown((KeyCode.DownArrow)))
+		if (Input.GetKey((KeyCode.DownArrow)))
 		{
 			transform.position = new Vector3(transform.position.x,transform.position.y - Speed, transform.position.z);
 		}
 
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.position = new Vector3(transform.position.x - Speed, transform.position.y, transform.position.z);
+			//transform.position = new Vector3(transform.position.x - Speed, transform.position.y, transform.position.z);
 		}
 
-		if (Input.GetKeyDown(KeyCode.RightArrow))
+		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.position = new Vector3(transform.position.x + Speed, transform.position.y,transform.position.z);
+			//transform.position = new Vector3(transform.position.x + Speed, transform.position.y,transform.position.z);
 		}
 		if( Input.GetKeyDown(KeyCode.R) )
 		{
@@ -48,6 +48,11 @@ public class Player1 : MonoBehaviour
 		{
 			Count = Count + 1;
 			CountText.text = "Score:" + Count.ToString();
+			transform.position = new Vector3(6.85f,-3.79f,0);
+		}
+
+		if (transform.position.y < -3.79f)
+		{
 			transform.position = new Vector3(6.85f,-3.79f,0);
 		}
 
--- Freeway Assignment/Assets/Player2.cs
+++ Freeway Assignment/Assets/Player2.cs
@@ -20,24 +20,24 @@ public class Player2 : MonoBehaviour
 	
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyDown("w"))
+        if (Input.GetKey("w"))
         {
             transform.position = new Vector3(transform.position.x,transform.position.y + Speed, transform.position.z);
         }
 
-        if (Input.GetKeyDown("s"))
+        if (Input.GetKey("s"))
         {
             transform.position = new Vector3(transform.position.x,transform.position.y - Speed, transform.position.z);
         }
 
         if (Input.GetKeyDown("a"))
         {
-            transform.position = new Vector3(transform.position.x - Speed, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(transform.position.x - Speed, transform.position.y, transform.position.z);
         }
 
         if (Input.GetKeyDown("d"))
         {
-            transform.position = new Vector3(transform.position.x + Speed, transform.position.y,transform.position.z);
+            //transform.position = new Vector3(transform.position.x + Speed, transform.position.y,transform.position.z);
         }
 
         if (transform.position.y >= 14)
@@ -46,11 +46,23 @@ public class Player2 : MonoBehaviour
             Score = Score + 1;
             ScoreText.text = "Score:" + Score.ToString();
         }
+
+        if (transform.position.y < -3.748f)
+        {
+            transform.position = new Vector3(-10.45f,-3.748f,0);
+        }
         if (Score == 5)
         {
             ScoreText.text = "Player 2 Wins";
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Car"))
+        {
+            transform.position = new Vector3(transform.position.x, -3.86f, transform.position.z);
+        }
     }
 }
