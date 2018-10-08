db1784999a191df69f644ade0084e33fd4dc2cda
LAPTOP-1L0MPP9Q\demok <ch3067@nyu.edu>
2018-10-08 16:15:35 -0400

    Viking Game Commit

---
+++ Freeway Assignment/Assets/Scripts/Player1.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(AudioSource))]
+public class Player1 : MonoBehaviour
+{
+	public Text GameOverText;
+	public float Speed;
+	public Canvas Canvas;
+	public Text CountText;
+	public int Count;
+	public Animator Tread;
+	public AudioSource Audio;
+	void Start ()
+	{
+		SceneManager.GetActiveScene();
+		Count = 0;
+		CountText.text = "Score:" + Count.ToString();
+		Tread = GetComponent<Animator>();
+	    Audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position = new Vector3(transform.position.x,transform.position.y + Speed, transform.position.z);
+			Tread.SetInteger("State",1);
+			
+		}
+		else
+		{
+			Tread.SetInteger("State",0);
+		}
+
+		if (Input.GetKey((KeyCode.DownArrow)))
+		{
+			transform.position = new Vector3(transform.position.x,transform.position.y - Speed, transform.position.z);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			//transform.position = new Vector3(transform.position.x - Speed, transform.position.y, transform.position.z);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			//transform.position = new Vector3(transform.position.x + Speed, transform.position.y,transform.position.z);
+		}
+		if( Input.GetKeyDown(KeyCode.R) )
+		{
+			SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+		}
+
+		if (transform.position.y >= 14)
+		{
+			Count = Count + 1;
+			CountText.text = "Score:" + Count.ToString();
+			transform.position = new Vector3(6.85f,-3.79f,0);
+			Audio.Play();
+		}
+
+		if (transform.position.y < -3.79f)
+		{
+			transform.position = new Vector3(6.85f,-3.79f,0);
+		}
+
+		if (Count == 5)
+		{
+			CountText.text = "Player 1 Wins";
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Car"))
+		{
+		transform.position = new Vector3(transform.position.x, -3.86f, transform.position.z);
+		}
+	}
+
+}
